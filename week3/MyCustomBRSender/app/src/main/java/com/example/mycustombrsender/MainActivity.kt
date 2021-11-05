8b4c01949c79df3d07d7b90a92297fa766c3a20e
oxojjjj <oxojjjj@gmail.com>
2021-11-05 20:01:07 +0900

    feat: sender broadcast example

---
+++ week3/MyCustomBRSender/app/src/main/java/com/example/mycustombrsender/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mycustombrsender
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mycustombrsender.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val MY_ACTION = "com.oseong.action.EXAMPLE_ACTION"
+        const val EXTRA_KEY = "com.oseong.key.EXTRA_TEXT"
+    }
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btSent.setOnClickListener {
+            Intent().let {
+                it.action = MY_ACTION
+                it.putExtra(EXTRA_KEY, binding.etText.text.toString())
+                sendBroadcast(it)
+            }
+        }
+    }
+}
