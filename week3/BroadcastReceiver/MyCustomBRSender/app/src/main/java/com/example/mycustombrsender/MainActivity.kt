d58e50ba819baf9fa4efab91433727745ece6e89
Oseong Kwon <oxojjjj@gmail.com>
2021-11-06 11:20:23 +0900

    feat: 퍼미션 추가

--- week3/BroadcastReceiver/MyCustomBRSender/app/src/main/java/com/example/mycustombrsender/MainActivity.kt
+++ week3/BroadcastReceiver/MyCustomBRSender/app/src/main/java/com/example/mycustombrsender/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.mycustombrsender.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     companion object {
         const val MY_ACTION = "com.oseong.action.EXAMPLE_ACTION"
+        const val MY_PERMISSION = "com.oseong.permission.EXAMPLE_ACTION_PERMISSION"
         const val EXTRA_KEY = "com.oseong.key.EXTRA_TEXT"
     }
 
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
             Intent().let {
                 it.action = MY_ACTION
                 it.putExtra(EXTRA_KEY, binding.etText.text.toString())
-                sendBroadcast(it)
+                sendBroadcast(it, MY_PERMISSION)
             }
         }
     }
