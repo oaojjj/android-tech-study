60c86f68fa88297bdc4d4cfd3a80bd418d9be1f3
oaojjj <oxojjjj@gmail.com>
2021-11-12 20:25:59 +0900

    feat: add JobScheduler Example

--- week4/JobSchedulerExample/app/src/main/java/com/example/jobschedulerexample/MainActivity.kt
+++ week4/JobSchedulerExample/app/src/main/java/com/example/jobschedulerexample/MainActivity.kt
@@ -1,11 +1,43 @@
- package com.example.jobschedulerexample
+package com.example.jobschedulerexample
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import com.example.jobschedulerexample.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val JOB_ID = 7
+        private const val TAG = "MainActivity"
+    }
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        val componentName = ComponentName(this, MyJobService::class.java)
+        val jobInfo = JobInfo.Builder(JOB_ID, componentName)
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED )
+            .setExtras(PersistableBundle().apply { putString("EXTRA_KEY", "JobSchedulerExample") })
+            .build()
+
+        binding.btnStart.setOnClickListener {
+            Log.d(TAG, "onCreate: JobScheduler Start $JOB_ID")
+            jobScheduler.schedule(jobInfo)
+        }
+
+        binding.btnStop.setOnClickListener {
+            Log.d(TAG, "onCreate: JobScheduler Stop $JOB_ID")
+            jobScheduler.cancel(JOB_ID)
+        }
     }
 }
