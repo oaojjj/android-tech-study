bfe76313c41457c70bddc79ab7d2e57105db604c
Oseong Kwon <oxojjjj@gmail.com>
2021-11-13 18:59:33 +0900

    feat: add WorkManagerExample

--- week4/WorkManagerExample/app/src/main/java/com/example/workmanagerexample/MainActivity.kt
+++ week4/WorkManagerExample/app/src/main/java/com/example/workmanagerexample/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.workmanagerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.workmanagerexample.databinding.ActivityMainBinding
+import com.example.workmanagerexample.workers.BlurWorker
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var workManager: WorkManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.goButton.setOnClickListener {
+            workManager = WorkManager.getInstance(applicationContext)
+            workManager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
+        }
     }
 }
